69526f4da5d407d3b6d29d8c2b9297b76b52b95d
Tudor-Alexandru-B4 <tudor.gherghe@yahoo.com>
2022-10-16 15:13:32 +0300

    Throw Handled

--- Lab2/VR Balloon Popping/Assets/Scripts/HedgehogController.cs
+++ Lab2/VR Balloon Popping/Assets/Scripts/HedgehogController.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class HedgehogController : MonoBehaviour
 {
+    [SerializeField]
+    float distanceFromSpawnPoint;
+    bool spawnedNewHedgehog = false;
     GameObject mainCamera;
+    Vector3 spawnPosition;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnPosition = transform.position;
         mainCamera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!spawnedNewHedgehog && Vector3.Distance(spawnPosition, transform.position) > distanceFromSpawnPoint)
+        {
+            SpawnNewHedgehog();
+        }
         transform.LookAt(new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z));
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            if (!spawnedNewHedgehog)
+            {
+                SpawnNewHedgehog();
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    private void SpawnNewHedgehog()
+    {
+        spawnedNewHedgehog = true;
+        GameObject.Find("HedgehogSpawner").GetComponent<HedgehogSpawnerController>().SpawnHedgehog();
+    }
+
 }
